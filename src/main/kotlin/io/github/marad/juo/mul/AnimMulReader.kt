d6a6bebecb16fdf39d245f46b4f3e7ffa93b6ffd
marad <moriturius@gmail.com>
2018-08-19 15:14:32 +0200

    Removed unused code

--- src/main/kotlin/io/github/marad/juo/mul/AnimMulReader.kt
+++ src/main/kotlin/io/github/marad/juo/mul/AnimMulReader.kt
@@ -2,11 +2,6 @@ package io.github.marad.juo.mul
 
 import java.io.DataInputStream
 
-data class Frame(val imageCenterX: Short, val imageCenterY: Short,
-                 val width: Short, val height: Short)
-
-private data class FrameChunk(val xOffset: Short, val yOffset: Short, val pixelIndices: ByteArray)
-
 class AnimMulReader(private val indexedMulFile: IndexedMulFile) {
     fun getAnimation(animId: Int): List<Image> {
         val index = indexedMulFile.getIndex(animId)
@@ -61,14 +56,8 @@ class AnimMulReader(private val indexedMulFile: IndexedMulFile) {
             }
             val runLength = (rowHeader and 0x0FFF).toShort()
             val lineNum = ((rowHeader shr 12) and 0x000F)
-//            val unknown = (rowOffset and 0x03f)
-//            val tmp = (rowOffset and 0x8000)
-//            rowOffset = tmp or (rowOffset shr 6)
             rowOffset = rowOffset shr 6
 
-            println("runLength: $runLength")
-            println("yOffset: $lineNum")
-            println("xOffset: $rowOffset")
             val x = imageCenterX + rowOffset
             if (prevLineNum != 0xFF && lineNum != prevLineNum) {
                 y++
@@ -88,52 +77,7 @@ class AnimMulReader(private val indexedMulFile: IndexedMulFile) {
             }
         }
 
-//        println("cx: $imageCenterX cy: $imageCenterY")
-//        println("width: $width height: $height")
-//        println(readChunk(stream))
-//
-//        generateSequence { readChunk(stream) }
-//                .forEach { chunk ->
-//                    var x = imageCenterX + chunk.xOffset
-//                    val y = imageCenterY + chunk.yOffset
-//                    chunk.pixelIndices
-//                            .map { palette[it.toInt() and 0xFF] }
-//                            .forEach {
-//                                println("x: $x y: $y")
-//                                imageData[y * width + x] = it
-//                                x += 1
-//                            }
-//                }
-
         return Image(width.toInt(), height.toInt(), imageData)
     }
-
-//    private fun readChunk(stream: DataInputStream): FrameChunk? {
-//        val header = stream.readInt().toBigEndian()
-//        if (header == 0x7FFF7FFF) {
-//            return null
-//        }
-//        val pixelCount = header and 0x0FFF
-//        val yOffset = ((header ushr 12) and 0xF)
-//        val xOffset = ((header ushr 22) and 0x3FF)
-//
-//            println("runLength: $pixelCount")
-//            println("yOffset: $xOffset")
-//            println("xOffset: $yOffset")
-//
-//        val pixelIndices = ByteArray(pixelCount)
-//        stream.readNBytes(pixelIndices, 0, pixelCount)
-//
-//        return FrameChunk(xOffset.toShort(), yOffset.toShort(), pixelIndices)
-//    }
-
 }
 
-fun main(args: Array<String>) {
-    val reader = AnimMulReader(IndexedMulFile("D:\\Gry\\UO\\anim.idx", "D:\\Gry\\UO\\anim.mul"))
-    reader.getAnimation(110)
-
-//    val b: Byte = -1
-//    val i = b.toInt() and 0xFF
-//    println(i)
-}
