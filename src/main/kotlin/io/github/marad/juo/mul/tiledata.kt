7034bfd4e58a49a02dc573a3d1c5511d8ba89ff6
marad <moriturius@gmail.com>
2018-08-27 04:57:51 +0200

    Implementing tiledata flags

--- src/main/kotlin/io/github/marad/juo/mul/tiledata.kt
+++ src/main/kotlin/io/github/marad/juo/mul/tiledata.kt
@@ -4,13 +4,15 @@ import java.io.ByteArrayInputStream
 import java.io.DataInputStream
 import java.io.RandomAccessFile
 
+typealias Flags = Int
+
 data class LandEntry(
-        val flags: Int,
+        val flags: Flags,
         val textureId: Short,
         val name: String)
 
 data class StaticEntry(
-        val flags: Int,
+        val flags: Flags,
         val weight: Byte,
         val quality: Byte,
 //        val unknown1: Short,
@@ -87,6 +89,48 @@ class TiledataMulReader(private val file: RandomAccessFile) {
         staticEntries.forEach { println(it) }
     }
 
+}
+
+fun Flags.isBackground(): Boolean = (this and TiledataFlag.Background.value) == 1
+fun Flags.isWeapon(): Boolean = (this and TiledataFlag.Weapon.value) == 1
+fun Flags.isTransparent(): Boolean = (this and TiledataFlag.Transparent.value) == 1
+fun Flags.isTranslucent(): Boolean = (this and TiledataFlag.Translucent.value) == 1
+fun Flags.isWall(): Boolean = (this and TiledataFlag.Wall.value) == 1
+// TODO: reszta
+
+enum class TiledataFlag(val value: Int) {
+    Background(0x00000001),
+    Weapon(0x00000002),
+    Transparent(0x00000004),
+    Translucent(0x00000008),
+    Wall(0x00000010),
+    Damaging(0x00000020),
+    Impassable(0x00000040),
+    Wet(0x00000080),
+    Unknown1(0x00000100),
+    Surface(0x00000200),
+    Bridge(0x00000400),
+    Generic(0x00000800),
+    Window(0x00001000),
+    NoShoot(0x00002000),
+    ArticleA(0x00004000),
+    ArticleAn(0x00008000),
+    Internal(0x00010000),
+    Foliage(0x00020000),
+    PartialHue(0x00040000),
+    Unknown2(0x00080000),
+    Map(0x00100000),
+    Container(0x00200000),
+    Wearable(0x00400000),
+    LightSource(0x00800000),
+    Animation(0x01000000),
+    NoDiagonal(0x02000000),
+    Unknown3(0x04000000),
+    Armor(0x08000000),
+    Roof(0x10000000),
+    Door(0x20000000),
+    StairBack(0x40000000),
+    StairRight(0x80000000.toInt())
 }
 
 fun main(args: Array<String>) {
